41223aa4e8f55371d97dfe8207e42846070147c1
Max <45385195+MasterPlayer@users.noreply.github.com>
2021-12-19 20:28:26 +0300

    update for v1.5
    fix issue #19

---
+++ src_sw/single_tap_intr/app.c
@@ -0,0 +1,107 @@
+#include <stdio.h>
+#include "platform.h"
+#include "xil_printf.h"
+#include "axi_adxl.h"
+#include <time.h>
+#include <xscugic.h>
+
+#include <xparameters.h>
+
+
+int scugic_initialize(XScuGic *ptr, axi_adxl* adxl_ptr);
+void adxl_intr_handler(void *callback);
+
+int main(){
+
+	init_platform();
+
+	axi_adxl adxl_device;
+	XScuGic gic;
+
+	axi_adxl_init(&adxl_device, 0x40030000, 0x40040000);
+
+	scugic_initialize(&gic, &adxl_device);
+
+	int status = axi_adxl_enable(&adxl_device, 0x53, 10);
+	if (status != ADXL_OK){
+		printf("[MAIN] : enable device return code [%d]\r\n", status);
+	}
+
+	status = axi_adxl_calibration(&adxl_device);
+	if (status != ADXL_OK){
+		printf("[MAIN] : calibration device return code [%d]\r\n", status);
+	}
+
+	status = axi_adxl_disable_requesting(&adxl_device);
+	if (status != ADXL_OK){
+		xil_printf("[MAIN] : disable requests return code : [%d]\r\n", status);
+		return 0;
+	}
+
+	axi_adxl_set_int_map(&adxl_device, SINGLE_TAP, 0x00);
+	axi_adxl_set_dur(&adxl_device, 0x10);
+	axi_adxl_set_thresh_tap(&adxl_device, 4);
+	axi_adxl_set_latency(&adxl_device, 0x01);
+	axi_adxl_set_window(&adxl_device, 0x01);
+	axi_adxl_set_tap_axes_active(&adxl_device, TAP_AXES_TAP_X_EN_MASK | TAP_AXES_TAP_Y_EN_MASK);
+	axi_adxl_int_enable(&adxl_device, SINGLE_TAP);
+
+	while(1){
+
+    }
+    cleanup_platform();
+    return 0;
+}
+
+
+
+
+int scugic_initialize(XScuGic *ptr, axi_adxl* adxl_ptr){
+
+	int status = 0;
+
+    XScuGic_Config *cfg;
+
+    cfg = XScuGic_LookupConfig(XPAR_SCUGIC_0_DEVICE_ID);
+    if (!cfg){
+        return XST_FAILURE;
+    }
+
+    status = XScuGic_CfgInitialize(ptr, cfg, cfg->CpuBaseAddress);
+    if (status != XST_SUCCESS){
+        return XST_FAILURE;
+    }
+
+    XScuGic_SetPriorityTriggerType(ptr, XPAR_FABRIC_AXI_ADXL345_VHD_0_ADXL_IRQ_INTR, 0x00, 0x3);
+
+    status = XScuGic_Connect(ptr, XPAR_FABRIC_AXI_ADXL345_VHD_0_ADXL_IRQ_INTR, (Xil_InterruptHandler)adxl_intr_handler, adxl_ptr);
+    if (status != XST_SUCCESS){
+        return XST_FAILURE;
+    }
+
+    XScuGic_Enable(ptr, XPAR_FABRIC_AXI_ADXL345_VHD_0_ADXL_IRQ_INTR);
+
+    Xil_ExceptionRegisterHandler(XIL_EXCEPTION_ID_INT, (Xil_ExceptionHandler)XScuGic_InterruptHandler, ptr);
+    Xil_ExceptionEnable();
+
+    return status ;
+}
+
+
+
+void adxl_intr_handler(void *callback){
+	axi_adxl *ptr = (axi_adxl*)callback;
+
+	adxl_cfg_perform_request(ptr->cfg);
+
+	while(!adxl_cfg_has_request_performed(ptr->cfg)){
+
+	}
+	adxl_cfg_clear_request_performed(ptr->cfg);
+
+	g_coord g;
+	axi_adxl_get_gravity(ptr, &g);
+	printf("[x] : %3.3f \t[y] : %3.3f \t [z] %3.3f\r\n", g.x, g.y, g.z);
+	return;
+}
+
---
+++ src_sw/single_tap_intr/axi_adxl_cfg.h
@@ -0,0 +1,66 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdio.h>
+#include <xil_types.h>
+
+typedef struct {
+    uint32_t ctl_reg;
+    uint32_t request_interval_reg;
+    uint32_t data_width_reg;
+    uint32_t read_valid_count_reg;
+    uint32_t write_valid_count_reg;
+    uint32_t write_transactions_reg;
+    uint32_t read_transactions_reg;
+    uint32_t clk_period_reg;
+} adxl_cfg;
+
+
+#define CTL_RESET_MASK          	0x00000001
+#define CTL_ENABLE_MASK         	0x00000002
+#define CTL_ALLOW_IRQ_MASK      	0x00000004
+#define CTL_PERFORM_REQUEST_MASK 	0x00000008
+#define CTL_REQUEST_PERFORMED_MASK  0x00000040
+#define CTL_ON_WORK_MASK        	0x00000080
+#define CTL_IIC_ADDR_MASK       	0x00007F00
+#define CTL_LINK_ON_MASK        	0x00008000
+#define CTL_VERS_MINOR_MASK     	0x00FF0000
+#define CTL_VERS_MAJOR_MASK     	0xFF000000
+
+#define adxl_cfg_has_reset(ptr) ((ptr)->ctl_reg & CTL_RESET_MASK) ? TRUE : FALSE
+#define adxl_cfg_reset(ptr) ((ptr)->ctl_reg |= CTL_RESET_MASK)
+
+#define adxl_cfg_has_enable(ptr) ((ptr)->ctl_reg & CTL_ENABLE_MASK) ? TRUE : FALSE
+#define adxl_cfg_enable(ptr) ((ptr)-> ctl_reg |= CTL_ENABLE_MASK)
+#define adxl_cfg_disable(ptr) ((ptr)->ctl_reg &= ~CTL_ENABLE_MASK)
+
+#define adxl_cfg_has_allow_irq(ptr) ((ptr)->ctl_reg & CTL_ALLOW_IRQ_MASK) ? TRUE : FALSE
+#define adxl_cfg_allow_irq(ptr) ((ptr)->ctl_reg |= CTL_ALLOW_IRQ_MASK)
+#define adxl_cfg_unallow_irq(ptr) ((ptr)->ctl_reg &= ~CTL_ALLOW_IRQ_MASK)
+
+#define adxl_cfg_perform_request(ptr) ((ptr)->ctl_reg |= CTL_PERFORM_REQUEST_MASK)
+
+#define adxl_cfg_has_request_performed(ptr) ((ptr)->ctl_reg & CTL_REQUEST_PERFORMED_MASK)
+#define adxl_cfg_clear_request_performed(ptr) ((ptr)->ctl_reg |= CTL_REQUEST_PERFORMED_MASK)
+
+#define adxl_cfg_has_on_work(ptr) ((ptr)->ctl_reg & CTL_ON_WORK_MASK) ? TRUE : FALSE
+
+#define adxl_cfg_get_i2c_addr(ptr) (((ptr)->ctl_reg & CTL_IIC_ADDR_MASK) >> 8)
+#define adxl_cfg_set_i2c_addr(ptr, addr) ((ptr)->ctl_reg = (((ptr)->ctl_reg & ~CTL_IIC_ADDR_MASK) + ((uint32_t)addr<<8)))
+
+#define adxl_cfg_has_link(ptr) ((ptr)->ctl_reg & CTL_LINK_ON_MASK) ? TRUE : FALSE
+
+#define adxl_cfg_get_version_minor(ptr) (((ptr)->ctl_reg & CTL_VERS_MINOR_MASK) >> 16)
+#define adxl_cfg_get_version_major(ptr) (((ptr)->ctl_reg & CTL_VERS_MAJOR_MASK) >> 24)
+
+#define adxl_cfg_get_request_interval(ptr) ((ptr)->request_interval_reg)
+#define adxl_cfg_set_request_interval(ptr, value) ((ptr)->request_interval_reg = value)
+
+#define adxl_cfg_get_data_width(ptr) ((ptr)->data_width_reg)
+#define adxl_cfg_get_read_valid_count(ptr) ((ptr)->read_valid_count_reg)
+#define adxl_cfg_get_write_valid_count(ptr) ((ptr)->write_valid_count_reg)
+#define adxl_cfg_get_write_transactions(ptr) ((ptr)->write_transactions_reg)
+#define adxl_cfg_get_read_transactions(ptr) ((ptr)->read_transactions_reg)
+#define adxl_cfg_get_clk_period(ptr) ((ptr)->clk_period_reg)
+
+void adxl_cfg_debug(adxl_cfg *cfg_ptr);
