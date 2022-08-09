cf1780cab73c3c64b370e4075dfe3a490c225348
Maxim <masterplayer@yandex.ru>
2022-08-09 20:18:36 +0300

    Update adxl345_functional.sv

--- src_hw/adxl345_functional.sv
+++ src_hw/adxl345_functional.sv
@@ -150,7 +150,15 @@ module adxl345_functional (
                 if (WVALID & WSTRB[index]) begin
                     need_update_reg[WADDR][index] <= write_mask_register[WADDR][index];
                 end else begin
-                    need_update_reg[WADDR][index] <= need_update_reg[WADDR][index];
+                    if (index == write_memory_lo) begin 
+                        if (current_state == UPD_CHK_FLAQ_ST) begin 
+                            need_update_reg[write_memory_hi][index] <= 1'b0;
+                        end else begin 
+                            need_update_reg[write_memory_hi][index] <= need_update_reg[write_memory_hi][index];
+                        end 
+                    end else begin 
+                        need_update_reg[WADDR][index] <= need_update_reg[WADDR][index];
+                    end 
                 end
             end 
 
@@ -301,6 +309,51 @@ module adxl345_functional (
     );
 
 
+    xpm_fifo_sync #(
+        .CASCADE_HEIGHT     (0       ), // DECIMAL
+        .DOUT_RESET_VALUE   ("0"     ), // String
+        .ECC_MODE           ("no_ecc"), // String
+        .FIFO_MEMORY_TYPE   ("auto"  ), // String
+        .FIFO_READ_LATENCY  (1       ), // DECIMAL
+        .FIFO_WRITE_DEPTH   (2048    ), // DECIMAL
+        .FULL_RESET_VALUE   (0       ), // DECIMAL
+        .PROG_EMPTY_THRESH  (10      ), // DECIMAL
+        .PROG_FULL_THRESH   (10      ), // DECIMAL
+        .RD_DATA_COUNT_WIDTH(1       ), // DECIMAL
+        .READ_DATA_WIDTH    (32      ), // DECIMAL
+        .READ_MODE          ("std"   ), // String
+        .SIM_ASSERT_CHK     (0       ), // DECIMAL; 0=disable simulation messages, 1=enable simulation messages
+        .USE_ADV_FEATURES   ("0707"  ), // String
+        .WAKEUP_TIME        (0       ), // DECIMAL
+        .WRITE_DATA_WIDTH   (32      ), // DECIMAL
+        .WR_DATA_COUNT_WIDTH(1       )  // DECIMAL
+    ) xpm_fifo_sync_inst (
+        .almost_empty (almost_empty ), // 1-bit output: Almost Empty : When asserted, this signal indicates that
+        .almost_full  (almost_full  ), // 1-bit output: Almost Full: When asserted, this signal indicates that
+        .data_valid   (             ), // 1-bit output: Read Data Valid: When asserted, this signal indicates
+        .dbiterr      (             ), // 1-bit output: Double Bit Error: Indicates that the ECC decoder detected
+        .dout         (dout         ), // READ_DATA_WIDTH-bit output: Read Data: The output data bus is driven
+        .empty        (empty        ), // 1-bit output: Empty Flag: When asserted, this signal indicates that the
+        .full         (full         ), // 1-bit output: Full Flag: When asserted, this signal indicates that the
+        .overflow     (overflow     ), // 1-bit output: Overflow: This signal indicates that a write request
+        .prog_empty   (prog_empty   ), // 1-bit output: Programmable Empty: This signal is asserted when the
+        .prog_full    (prog_full    ), // 1-bit output: Programmable Full: This signal is asserted when the
+        .rd_data_count(rd_data_count), // RD_DATA_COUNT_WIDTH-bit output: Read Data Count: This bus indicates the
+        .rd_rst_busy  (rd_rst_busy  ), // 1-bit output: Read Reset Busy: Active-High indicator that the FIFO read
+        .sbiterr      (sbiterr      ), // 1-bit output: Single Bit Error: Indicates that the ECC decoder detected
+        .underflow    (underflow    ), // 1-bit output: Underflow: Indicates that the read request (rd_en) during
+        .wr_ack       (wr_ack       ), // 1-bit output: Write Acknowledge: This signal indicates that a write
+        .wr_data_count(wr_data_count), // WR_DATA_COUNT_WIDTH-bit output: Write Data Count: This bus indicates
+        .wr_rst_busy  (wr_rst_busy  ), // 1-bit output: Write Reset Busy: Active-High indicator that the FIFO
+        .din          (din          ), // WRITE_DATA_WIDTH-bit input: Write Data: The input data bus used when
+        .injectdbiterr(1'b0         ), // 1-bit input: Double Bit Error Injection: Injects a double bit error if
+        .injectsbiterr(1'b0         ), // 1-bit input: Single Bit Error Injection: Injects a single bit error if
+        .rd_en        (rd_en        ), // 1-bit input: Read Enable: If the FIFO is not empty, asserting this
+        .rst          (rst          ), // 1-bit input: Reset: Must be synchronous to wr_clk. The clock(s) can be
+        .sleep        (sleep        ), // 1-bit input: Dynamic power saving- If sleep is High, the memory/fifo
+        .wr_clk       (wr_clk       ), // 1-bit input: Write clock: Used for write operation. wr_clk must be a
+        .wr_en        (wr_en        )  // 1-bit input: Write Enable: If the FIFO is not full, asserting this
+    );
 
     xpm_memory_sdpram #(
         .ADDR_WIDTH_A           (6              ), // DECIMAL
